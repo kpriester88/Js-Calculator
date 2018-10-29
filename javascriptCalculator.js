d59eb4a73bc43ad0db766f2580f20d3841b1fdee
kpriester88 <kpriester88@gmail.com>
2018-10-29 13:56:04 -0400

    Update javascriptCalculator.js

--- javascriptCalculator.js
+++ javascriptCalculator.js
@@ -2,7 +2,6 @@ function calc()
 	{
 		var n1 = parseFloat(document.getElementById("n1").value);
 		var n2 = parseFloat(document.getElementById("n2").value);
-	
 		var oper = document.getElementById('operators').value;
 
 
